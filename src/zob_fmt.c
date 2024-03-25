07ffa2f679ecdb3822819d99c99331ba554bc6a7
mattlianje <matthieu.court@gmail.com>
2024-03-25 00:36:11 -0400

    feat: add the zob_fmt files

---
+++ src/zob_fmt.c
@@ -0,0 +1,48 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "config.h"
+#include "zob_fmt.h"
+
+void runFmt(int argc, char **argv) {
+     char filename[256] = {0};
+
+     if (argc < 2) {
+          system("clear || cls");
+          printf("「Z O B」- Enter the file name: ");
+          if (scanf("%255s", filename) != 1) {
+               fprintf(stderr, "「Z O B」- Error reading filename.\n");
+               return;
+          }
+     } else {
+          strncpy(filename, argv[1], sizeof(filename) - 1);
+     }
+
+     const char *fileExtension = strrchr(filename, '.');
+     if (!fileExtension) {
+          fprintf(stderr, "「Z O B」- File extension not found.\n");
+          return;
+     }
+
+     bool commandExecuted = false;
+     for (int i = 0; i < sizeof(LINTER_MAPPING) / sizeof(LINTER_MAPPING[0]); ++i) {
+          if (strcmp(fileExtension + 1, LINTER_MAPPING[i][0]) == 0) {
+               system("clear || cls");
+               char command[LINTER_CMD_MAX_SIZE];
+               snprintf(command, sizeof(command), LINTER_MAPPING[i][1], filename);
+               int status = system(command);
+               if (status != -1) {
+                    printf("「Z O B」- ✨ %s ✨ has been cleansed.\n", filename);
+                    commandExecuted = true;
+                    break;
+               }
+          }
+     }
+
+     if (!commandExecuted) {
+          fprintf(stderr, "「Z O B」- No linter configuration found for the given file type.\n");
+     }
+}
---
+++ src/zob_fmt.h
@@ -0,0 +1,6 @@
+#ifndef ZOB_FMT_H
+#define ZOB_FMT_H
+
+void runFmt(int argc, char **argv);
+
+#endif // ZOB_FMT_H
