11d9fa71fd062dd959f5cddb0bda250ff8bc3869
Matthieu Court <matthieu.court@instacart.com>
2024-05-11 02:20:37 -0400

    feat: refactor zob tex

--- src/zob_tex.c
+++ src/zob_tex.c
@@ -1,14 +1,50 @@
-#include "zob_tex.h"
-
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 #include "config.h"
+#include "zob_tex.h"
 
-/* Prototypes */
-void runTex(int argc, char **argv);
+typedef enum {
+     TOKEN_TEXT,
+     TOKEN_HEADER1,
+     TOKEN_HEADER2,
+     TOKEN_BOLD,
+     TOKEN_ITALIC,
+     TOKEN_LINK,
+     TOKEN_LIST_ITEM,
+     TOKEN_CODE_BLOCK,
+     TOKEN_NEWLINE
+} TokenType;
+
+typedef struct Token {
+     TokenType type;
+     char *content;
+     struct Token *next;
+} Token;
+
+Token *createToken(TokenType type, const char *content) {
+     Token *token = malloc(sizeof(Token));
+     if (token == NULL) return NULL;
+     token->type = type;
+     token->content = strdup(content);
+     if (token->content == NULL) {
+          free(token);
+          return NULL;
+     }
+     token->next = NULL;
+     return token;
+}
+
+void freeTokens(Token *token) {
+     while (token) {
+          Token *next = token->next;
+          free(token->content);
+          free(token);
+          token = next;
+     }
+}
 
 char *readFileIntoString(const char *filename) {
      FILE *file = fopen(filename, "r");
@@ -35,147 +71,270 @@ char *readFileIntoString(const char *filename) {
      return buffer;
 }
 
-void processMarkdownLine(const char *line, bool *insideList, char **output) {
-     char *out = *output;
+Token *tokenizeMarkdown(const char *markdown) {
+     Token *head = NULL;
+     Token **current = &head;
+     bool insideCodeBlock = false;
 
-     // Titles and subtitles
-     if (strncmp(line, "# ", 2) == 0) {
-          out += sprintf(out, "\\section{%s}\n\n", line + 2);
-     } else if (strncmp(line, "## ", 3) == 0) {
-          out += sprintf(out, "\\subsection{%s}\n\n", line + 3);
-     }
-     // List items
-     else if (strncmp(line, "- ", 2) == 0) {
-          if (!(*insideList)) {
-               out += sprintf(out, "\\begin{itemize}\n");
-               *insideList = true;
-          }
-          out += sprintf(out, "  \\item %s\n", line + 2);
-     }
-     // Bold and italic
-     else {
-          // Simple replacement for **bold** and __italic__
-          // TODO: Implement the colour boxes
-          const char *ptr = line;
-          while (*ptr) {
-               if (strncmp(ptr, "**", 2) == 0) {
-                    out += sprintf(out, "\\textbf{");
-                    ptr += 2;  // Skip the marker
-                    while (*ptr && strncmp(ptr, "**", 2) != 0) {
-                         *out++ = *ptr++;
-                    }
-                    if (*ptr) ptr += 2;  // Skip the closing
-                    *out++ = '}';
-               } else if (strncmp(ptr, "__", 2) == 0) {
-                    out += sprintf(out, "\\textit{");
-                    ptr += 2;  // Skip the marker
-                    while (*ptr && strncmp(ptr, "__", 2) != 0) {
-                         *out++ = *ptr++;
-                    }
-                    if (*ptr) ptr += 2;  // Skip the closing
-                    *out++ = '}';
-               } else {
-                    *out++ = *ptr++;
+     for (const char *ptr = markdown; *ptr;) {
+          if (insideCodeBlock) {
+               if (strncmp(ptr, "```", 3) == 0) {
+                    ptr += 3;
+                    insideCodeBlock = false;
+                    continue;
                }
+               const char *start = ptr;
+               while (*ptr && strncmp(ptr, "```", 3) != 0) ptr++;
+               *current = createToken(TOKEN_CODE_BLOCK, strndup(start, ptr - start));
+               if (!*current) return head;
+               current = &(*current)->next;
+               continue;
           }
-          *out++ = '\n';
+
+          /* When we enter a code block */
+          if (strncmp(ptr, "```", 3) == 0) {
+               ptr += 3;
+               insideCodeBlock = true;
+               continue;
+          }
+
+          if (*ptr == '[') {
+               ptr++;
+               const char *text_start = ptr;
+               while (*ptr && *ptr != ']') ptr++;
+               char *link_text = strndup(text_start, ptr - text_start);
+               ptr++; /* Skip the closing ] */
+               ptr++; /* Skip the opening ( */
+               const char *url_start = ptr;
+               while (*ptr && *ptr != ')') ptr++;
+               char *url = strndup(url_start, ptr - url_start);
+               ptr++; /* Skip the closing ) */
+
+               char *full_link = malloc(strlen(link_text) + strlen(url) + 32);
+               sprintf(full_link, "\\href{%s}{%s}", url, link_text);
+               *current = createToken(TOKEN_LINK, full_link);
+               free(link_text);
+               free(url);
+               free(full_link);
+               /* TODO better handle malloc failure */
+               if (!*current) return head;
+               current = &(*current)->next;
+               continue;
+          }
+
+          const char *start = ptr;
+          TokenType type = TOKEN_TEXT;
+          size_t length = 0;
+
+          if (*ptr == '\n') {
+               ptr++;
+               continue;
+          }
+
+          if (strncmp(ptr, "# ", 2) == 0) {
+               ptr += 2;
+               start = ptr;
+               type = TOKEN_HEADER1;
+               while (*ptr && *ptr != '\n') ptr++;
+               length = ptr - start;
+          } else if (strncmp(ptr, "## ", 3) == 0) {
+               ptr += 3;
+               start = ptr;
+               type = TOKEN_HEADER2;
+               while (*ptr && *ptr != '\n') ptr++;
+               length = ptr - start;
+          } else if (strncmp(ptr, "**", 2) == 0) {
+               ptr += 2;
+               start = ptr;
+               type = TOKEN_BOLD;
+               while (*ptr && strncmp(ptr, "**", 2) != 0) ptr++;
+               length = ptr - start;
+               ptr += 2; /* Skip the closing ** */
+          } else if (*ptr == '*') {
+               ptr++;
+               start = ptr;
+               type = TOKEN_ITALIC;
+               while (*ptr && *ptr != '*') ptr++;
+               length = ptr - start;
+               ptr++; /* Skip the closing * */
+          } else if (strncmp(ptr, "- ", 2) == 0) {
+               ptr += 2;
+               start = ptr;
+               type = TOKEN_LIST_ITEM;
+               while (*ptr && *ptr != '\n') ptr++;
+               length = ptr - start;
+          } else {
+               start = ptr;
+               while (*ptr && *ptr != '\n' && *ptr != '*' && strncmp(ptr, "**", 2) != 0 &&
+                      *ptr != '[' && strncmp(ptr, "```", 3) != 0 && strncmp(ptr, "- ", 2) != 0 &&
+                      *ptr != '#')
+                    ptr++;
+               length = ptr - start;
+          }
+
+          *current = createToken(type, strndup(start, length));
+          if (!*current) return head;
+          current = &(*current)->next;
+
+          if (*ptr) ptr++; /* Move past the last processed character */
      }
-     *output = out;
+
+     return head;
 }
 
-char *replaceMarkdownSyntax(const char *markdown) {
-     char *output =
-         malloc(strlen(markdown) * 5);  // Should be enough for LaTeX syntax
-     if (output == NULL) {
-          fprintf(stderr, "Memory allocation error.\n");
-          return NULL;
+char *convertTokensToLatex(Token *tokens) {
+     size_t totalLength = 0;
+     Token *token;
+
+     for (token = tokens; token; token = token->next) {
+          switch (token->type) {
+               case TOKEN_HEADER1:
+               case TOKEN_HEADER2:
+               case TOKEN_BOLD:
+               case TOKEN_ITALIC:
+               case TOKEN_LINK:
+               case TOKEN_LIST_ITEM:
+               case TOKEN_CODE_BLOCK:
+               case TOKEN_TEXT:
+               case TOKEN_NEWLINE:
+                    totalLength += strlen(token->content) + 50;
+                    break;
+          }
      }
 
-     const char *line = markdown;
-     char *outPtr = output;
+     /* +1 for null terminator */
+     char *latex = malloc(totalLength + 1);
+     if (!latex) return NULL;
+     char *current = latex;
      bool insideList = false;
 
-     while (*line) {
-          const char *nextLine = strchr(line, '\n');
-          int lineLength = (nextLine) ? (nextLine - line) : strlen(line);
+     for (token = tokens; token; token = token->next) {
+          if (current != latex &&
+              (token->type == TOKEN_HEADER1 || token->type == TOKEN_HEADER2 ||
+               token->type == TOKEN_LIST_ITEM || token->type == TOKEN_CODE_BLOCK ||
+               token->type == TOKEN_LINK || token->type == TOKEN_NEWLINE)) {
+               current += sprintf(current, "\n");
+          }
 
-          char *lineBuffer = malloc(lineLength + 1);
-          strncpy(lineBuffer, line, lineLength);
-          lineBuffer[lineLength] = '\0';
-
-          processMarkdownLine(lineBuffer, &insideList, &outPtr);
-
-          free(lineBuffer);
-
-          if (nextLine)
-               line = nextLine + 1;
-          else
-               break;
+          switch (token->type) {
+               case TOKEN_HEADER1:
+               case TOKEN_HEADER2:
+                    if (insideList) {
+                         current += sprintf(current, "\\end{itemize}\n");
+                         insideList = false;
+                    }
+                    current += sprintf(
+                        current,
+                        token->type == TOKEN_HEADER1 ? "\n\\section{%s}\n" : "\n\\subsection{%s}\n",
+                        token->content);
+                    break;
+               case TOKEN_BOLD:
+                    current += sprintf(current, "\\textbf{%s} ", token->content);
+                    break;
+               case TOKEN_ITALIC:
+                    current += sprintf(current, "\\textit{%s} ", token->content);
+                    break;
+               case TOKEN_LINK:
+                    current += sprintf(current, "\\href{http://example.com}{%s}", token->content);
+                    break;
+               case TOKEN_LIST_ITEM:
+                    if (!insideList) {
+                         current += sprintf(current, "\\begin{itemize}\n");
+                         insideList = true;
+                    }
+                    current += sprintf(current, "\\item %s", token->content);
+                    if (token->next && token->next->type != TOKEN_LIST_ITEM) {
+                         current += sprintf(current, "\n\\end{itemize}\n");
+                         insideList = false;
+                    }
+                    break;
+               case TOKEN_CODE_BLOCK:
+                    if (insideList) {
+                         current += sprintf(current, "\\end{itemize}\n");
+                         insideList = false;
+                    }
+                    current +=
+                        sprintf(current, "\\begin{verbatim}\n%s\\end{verbatim}", token->content);
+                    break;
+               case TOKEN_NEWLINE:
+                    current += sprintf(current, "\n");
+                    break;
+               case TOKEN_TEXT:
+                    current += sprintf(current, "%s", token->content);
+                    break;
+          }
      }
 
      if (insideList) {
-          sprintf(outPtr, "\\end{itemize}\n");
+          current += sprintf(current, "\\end{itemize}\n");
      }
 
-     return output;
+     /* Null terminate the string */
+     *current = '\0';
+     return latex;
 }
 
 void runTex(int argc, char **argv) {
      char filename[256];
 
-     if (argc < 2) {
-          /* Prompt the user for a filename */
-          printf("Enter the Markdown file name: ");
-          if (scanf("%255s", filename) != 1) {
-               fprintf(stderr, "Error reading filename.\n");
-               return;
-          }
+     if (argc != 2) {
+          fprintf(stderr, "Usage: %s <markdown file>\n", argv[0]);
+          return;
+     }
 
-          size_t len = strlen(filename);
-          if (len < 3 || strcmp(filename + len - 3, ".md") != 0) {
-               fprintf(stderr, "The file must have a '.md' extension.\n");
-               return;
-          }
-     } else {
-          /* Using the provided argument as the filename */
-          strncpy(filename, argv[2], sizeof(filename));
-          filename[sizeof(filename) - 1] = '\0';  // Ensure null-termination
-
-          size_t len = strlen(filename);
-          if (len < 3 || strcmp(filename + len - 3, ".md") != 0) {
-               fprintf(stderr, "The file must have a '.md' extension.\n");
-               return;
-          }
+     strcpy(filename, argv[1]); /* Copies the file name from args */
+     size_t len = strlen(filename);
+     if (len < 3 || strcmp(filename + len - 3, ".md") != 0) {
+          fprintf(stderr, "The file must have a '.md' extension.\n");
+          return;
      }
 
      char *markdownContent = readFileIntoString(filename);
      if (markdownContent == NULL) {
+          fprintf(stderr, "Failed to read file.\n");
           return;
      }
 
-     /* Concatenating LATEX_PRELUDE, converted LaTeX content, and LATEX_END */
-     char *latexContent = replaceMarkdownSyntax(markdownContent);
-     if (latexContent != NULL) {
-          size_t totalLength = strlen(LATEX_PRELUDE) + strlen(latexContent) + strlen(LATEX_END) + 1;
-          char *finalContent = malloc(totalLength);
-          if (finalContent == NULL) {
-               fprintf(stderr, "Memory allocation error.\n");
-               free(latexContent);
-               free(markdownContent);
-               return;
-          }
-
-          snprintf(finalContent, totalLength, "%s%s%s", LATEX_PRELUDE, latexContent, LATEX_END);
-
-          printf("%s", finalContent);
-
-          free(finalContent);
-          free(latexContent);
-     } else {
-          fprintf(stderr, "Conversion failed.\n");
+     Token *tokens = tokenizeMarkdown(markdownContent);
+     if (!tokens) {
+          fprintf(stderr, "Failed to tokenize markdown.\n");
+          free(markdownContent);
+          return;
      }
 
-     system("clear || cls");
+     char *latexContent = convertTokensToLatex(tokens);
+     freeTokens(tokens);
      free(markdownContent);
+
+     if (latexContent) {
+          printf("%s\n", latexContent);
+          free(latexContent);
+     } else {
+          fprintf(stderr, "Failed to convert tokens to LaTeX.\n");
+     }
+}
+
+int main() {
+     const char *markdown =
+         "# Header1\n"
+         "This is some text **bold** *italic*.\n"
+         "- Item 1\n"
+         "- Item 2\n"
+         "Another paragraph.\n"
+         "[Link](http://example.com)\n"
+         "## Header2\n"
+         "Code block:\n"
+         "```\n"
+         "printf(\"Hello, world!\\n\");\n"
+         "```\n";
+
+     Token *tokens = tokenizeMarkdown(markdown);
+     char *latex = convertTokensToLatex(tokens);
+     if (latex) {
+          printf("%s\n", latex);
+          free(latex);
+     }
+     freeTokens(tokens);
+     return 0;
 }
 
